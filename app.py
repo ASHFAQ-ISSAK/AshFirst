0e5e6b1110ce8e04d1cfc990433555df9c1972c9
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-06-27 12:35:14 +0300

    added flask_restful resources

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from faker import Faker
 
@@ -6,6 +7,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+api = Api(app)
 fake = Faker()
 
 
@@ -46,137 +48,72 @@ class Order(db.Model):
 with app.app_context():
     db.create_all()
 
-    # Generate and save fake user data
-    for _ in range(10):  # Generate 10 users
-        name = fake.name()
-        email = fake.email()
-        user = User(name=name, email=email)
-        db.session.add(user)
-        db.session.commit()
-
-        # Generate and save fake order data for the user
-        for _ in range(3):  # Generate 3 orders per user
-            item_id = fake.random_int(min=1, max=10)  # Assuming 10 items
-            quantity = fake.random_int(min=1, max=5)  # Random quantity
-            order = Order(user_id=user.id, item_id=item_id, quantity=quantity)
-            db.session.add(order)
-    db.session.commit()
-
-    # Generate and save fake item data
-    for _ in range(10):  # Generate 10 items
-        name = fake.word()
-        price = fake.pyfloat(min_value=1, max_value=100, right_digits=2)
-        item = Item(name=name, price=price)
-        db.session.add(item)
-    db.session.commit()
+    # Generate and save fake data...
 
 
-@app.route("/users", methods=["GET"])
-def get_users():
-    users = User.query.all()
-    results = []
-    for user in users:
+# User resource
+class UserResource(Resource):
+    def get(self):
+        users = User.query.all()
+        results = []
+        for user in users:
+            orders = []
+            for order in user.orders:
+                item = Item.query.get(order.item_id)
+                orders.append(
+                    {"item": item.name, "price": item.price, "quantity": order.quantity}
+                )
+            results.append(
+                {
+                    "id": user.id,
+                    "name": user.name,
+                    "email": user.email,
+                    "orders": orders,
+                }
+            )
+        return results
+
+
+class UserDetailResource(Resource):
+    def get(self, user_id):
+        user = User.query.get(user_id)
+        if not user:
+            return {"error": "User not found"}, 404
+
         orders = []
         for order in user.orders:
             item = Item.query.get(order.item_id)
             orders.append(
                 {"item": item.name, "price": item.price, "quantity": order.quantity}
             )
-        results.append(
-            {"id": user.id, "name": user.name, "email": user.email, "orders": orders}
-        )
-    return jsonify(results)
+
+        return {"id": user.id, "name": user.name, "email": user.email, "orders": orders}
 
 
-@app.route("/users/<int:user_id>", methods=["GET"])
-def get_user(user_id):
-    user = User.query.get(user_id)
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-
-    print(f"User: {user.name}, {user.email}")
-    print("Orders:")
-    for order in user.orders:
-        print(
-            f"Item: {order.item.name}, Price: {order.item.price}, Quantity: {order.quantity}"
-        )
-
-    orders = []
-    for order in user.orders:
-        item = Item.query.get(order.item_id)
-        orders.append(
-            {"item": item.name, "price": item.price, "quantity": order.quantity}
-        )
-
-    return jsonify(
-        {"id": user.id, "name": user.name, "email": user.email, "orders": orders}
-    )
+api.add_resource(UserResource, "/users")
+api.add_resource(UserDetailResource, "/users/<int:user_id>")
 
 
-@app.route("/items", methods=["GET"])
-def get_items():
-    items = Item.query.all()
-    results = []
-    for item in items:
-        results.append({"id": item.id, "name": item.name, "price": item.price})
-    return jsonify(results)
+class ItemResource(Resource):
+    def get(self):
+        items = Item.query.all()
+        results = []
+        for item in items:
+            results.append({"id": item.id, "name": item.name, "price": item.price})
+        return results
 
 
-@app.route("/items/<int:item_id>", methods=["GET"])
-def get_item(item_id):
-    item = Item.query.get(item_id)
-    if not item:
-        return jsonify({"error": "Item not found"}), 404
+class ItemDetailResource(Resource):
+    def get(self, item_id):
+        item = Item.query.get(item_id)
+        if not item:
+            return {"error": "Item not found"}, 404
 
-    return jsonify({"id": item.id, "name": item.name, "price": item.price})
-
-
-@app.route("/items", methods=["POST"])
-def create_item():
-    data = request.get_json()
-    name = data.get("name")
-    price = data.get("price")
-
-    if not name or not price:
-        return jsonify({"error": "Invalid data"}), 400
-
-    item = Item(name=name, price=price)
-    db.session.add(item)
-    db.session.commit()
-
-    return jsonify({"message": "Item created successfully", "id": item.id}), 201
+        return {"id": item.id, "name": item.name, "price": item.price}
 
 
-@app.route("/items/<int:item_id>", methods=["PUT"])
-def update_item(item_id):
-    item = Item.query.get(item_id)
-    if not item:
-        return jsonify({"error": "Item not found"}), 404
-
-    data = request.get_json()
-    name = data.get("name")
-    price = data.get("price")
-
-    if not name or not price:
-        return jsonify({"error": "Invalid data"}), 400
-
-    item.name = name
-    item.price = price
-    db.session.commit()
-
-    return jsonify({"message": "Item updated successfully", "id": item.id})
-
-
-@app.route("/items/<int:item_id>", methods=["DELETE"])
-def delete_item(item_id):
-    item = Item.query.get(item_id)
-    if not item:
-        return jsonify({"error": "Item not found"}), 404
-
-    db.session.delete(item)
-    db.session.commit()
-
-    return jsonify({"message": "Item deleted successfully"})
+api.add_resource(ItemResource, "/items")
+api.add_resource(ItemDetailResource, "/items/<int:item_id>")
 
 
 if __name__ == "__main__":
